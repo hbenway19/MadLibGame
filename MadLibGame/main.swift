63b8392acc475e0492ce9e4430bdfa9932b8e9ad
Guest User <coder@mainlab5.local>
2016-01-12 11:30:05 -0800

    Names, in comments

--- MadLibGame/main.swift
+++ MadLibGame/main.swift
@@ -31,7 +31,7 @@ while correctInput == false {
         number = input()
     }
     
-} //menu - Hannah
+} //Hannah
 
 func mattsCode(){
 
@@ -53,7 +53,7 @@ var e = input()
     
 print("My company \(a) is developing \(b) to help \(c) \( d) with \(e)")
 }
-
+//matt
 
 
 
@@ -93,7 +93,7 @@ var o = input()
     
 print("He holds out his \(a), and in his \(b) are \(c) \(d) \(e) \(f) linked with a thick black thread ... Inside me! I \(g), and all the \(h) deep in my \(i) clench. My inner \(j) is doing the \(k) of the seven veils. It's a \(l) feeling. Once they're inside me, I cant really \(m) them-but then again I know they're there. I may have to \(n) these. They make me needy, needy for \(o)")
 }
-
+//matt
 
 
 func worldDomination(){
@@ -122,3 +122,4 @@ func worldDomination(){
     
     print("You are \(b) a evil villian. You are a villian becuase \(b) this must be accomplished. Your evil idea is \(i) this is your planto victory. Your evil base is in \(l) it has great defences and no one will expect it. Your evil nemeisis is \(n) he must be destroyed to dominate the world. Your ultimate weapon is \(s) this will cripple anyone who stands in your way. Your first target for world domination is \(y) this place will need to be in a good location. Your evil costume is \(c) This will be very intimidating. Your backup plan is \(p) This is needed in case you will not dominate the Earth. Your retirement plan is to \(r) This is after you have dominated the world. Congratsulations Villian you are ready for world domination go out there and beat \(n). This is a product of Juan coporation. Copyright 2016 all rights reserved ")
 }
+//Tiger
